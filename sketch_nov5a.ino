f8f34fcd62e519b48d8a2a82edb82094e3f0ea41
Abhyudaya Sharma <11471599+AbhyudayaSharma@users.noreply.github.com>
2023-11-05 23:00:12 -0500

    Move motor using button

--- sketch_nov5a.ino
+++ sketch_nov5a.ino
@@ -1,9 +1,26 @@
+#include <Servo.h>
+
+int isButtonPressed = LOW;
+Servo myservo;
+
 void setup() {
   // put your setup code here, to run once:
-
+  pinMode(6, INPUT);
+  myservo.attach(5);
+  Serial.begin(9600);
+  while (!Serial);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  int status = digitalRead(6);
 
+  // Serial.println(status);
+  if (status != isButtonPressed) {
+    isButtonPressed = status;
+    Serial.println(status == HIGH ? "Button pressed" : "Button released");
+  }
+
+  myservo.write(isButtonPressed == HIGH ? 180 : 0);
+  delay(100);
 }
